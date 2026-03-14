fe2236942f4d5e7ecdd47e8f24b1a4d30c800ba6
Oluwamayowa Oladosu <thenorthform@gmail.com>
2026-03-14 16:19:04 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
+
+export const metadata: Metadata = {
+  title: "Northform",
+  description: "Northform coming soon landing page",
+  icons: {
+    icon: [
+      {
+        url: "/images/vector_northform.svg",
+        type: "image/svg+xml",
+      },
+    ],
+    shortcut: "/images/vector_northform.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
